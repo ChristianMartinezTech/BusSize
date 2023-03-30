7c05283ec8747c28815e2aefd4f2b93d4b7b1b12
ChrisMartinezTech <crismartine.e@gmail.com>
2023-03-29 23:56:37 -0500

    finished the main function. Now I need to implement the lambda functionality

--- BusSize/src/main/java/org/example/BusSize.java
+++ BusSize/src/main/java/org/example/BusSize.java
@@ -1,13 +1,56 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-/***
- * 1. get the input from AWS Lambda
- * 2. Apply logic
- * 3. Return solution
- */
 public class BusSize {
-    // Test Lambda class
-    public String helloThere() {
-        return "Hello There!";
+    /***
+     * Method that will find all the possible ordered seat combinations
+     * @param groups integer[] containing the groups
+     * @return integer[] containing the possible ordered seat combinations
+     */
+    public Integer[] findChairSize(Integer[] groups) {
+
+        int highestValue = 0;
+        int passengerTotal = 0;
+        int temp = 0;
+        int noSeats;
+        ArrayList<Integer> responseArrList = new ArrayList<>();
+
+
+        for (Integer group : groups) { // Finding highestValue and passengerTotal
+            if (group > highestValue) {
+                highestValue = group;
+            }
+            passengerTotal += group;
+        }
+
+        noSeats = highestValue; // noSeats will go from the biggest group to half the total of passengers
+
+        while (noSeats <= (passengerTotal / 2)) {
+            for (Integer group : groups) {
+                if (group < noSeats) {
+                    temp += group;
+                }
+                if (temp == noSeats) {
+                    temp = 0;
+                }
+            }
+            if (temp == 0) {
+                responseArrList.add(noSeats); // Adding noSeats to responseArrList
+            } else {
+                temp = 0; // Restarting the temp count
+            }
+            noSeats += 1;
+        }
+
+        // Adding the last digit since it will always be total of passengers
+        responseArrList.add(passengerTotal);
+
+        // Changing the ArrayList into and Integer[]
+        Integer[] response = new Integer[responseArrList.size()];
+        response = responseArrList.toArray(response);
+        System.out.println(Arrays.toString(response));
+        return response;
     }
 }
+
--- BusSize/src/test/java/org/example/BusSizeTest.java
+++ BusSize/src/test/java/org/example/BusSizeTest.java
@@ -3,10 +3,13 @@ package org.example;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+
 public class BusSizeTest extends BusSize {
     @Test
     public void shouldReturnHelloThere() {
-        String hi = helloThere();
-        Assert.assertEquals("Hello There!", hi);
+        Integer[] groups = {1,2,1,1,1,2,1,3}; // Initializing groups
+        Integer[] response = findChairSize(groups); // Sending groups into findChairSize method
+        Assert.assertEquals(new Integer[]{3, 4, 6, 12}, response);
     }
 }
