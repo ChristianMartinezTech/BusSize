cd3f3c778a1864d20b83c4cd2b0ff6dc19c89e94
ChrisMartinezTech <crismartine.e@gmail.com>
2023-03-30 19:09:25 -0500

    Core logic implemented, now we need to implement JSON serialization and deserialization

--- BusSize/src/main/java/org/example/BusSize.java
+++ BusSize/src/main/java/org/example/BusSize.java
@@ -8,35 +8,34 @@ public class BusSize {
      * @param groups integer[] containing the groups
      * @return integer[] containing the possible ordered seat combinations
      */
-    public Integer[] findChairSize(Integer[] groups) {
-
+    public String findChairSize(String groups) {
         int highestValue = 0;
         int passengerTotal = 0;
         int temp = 0;
         int noSeats;
-        ArrayList<Integer> responseArrList = new ArrayList<>();
+        int[] numbers = Arrays.stream(groups.split(",")).mapToInt(Integer::parseInt).toArray();
+        String response = "";
 
-
-        for (int i = 0; i < groups.length; i++) { // Finding highestValue and passengerTotal
-            if (groups[i] > highestValue) {
-                highestValue = groups[i];
+        for (int i = 0; i < numbers.length; i++) { // Finding highestValue and passengerTotal
+            if (numbers[i] > highestValue) {
+                highestValue = numbers[i];
             }
-            passengerTotal += groups[i];
+            passengerTotal += numbers[i];
         }
 
         noSeats = highestValue; // noSeats will go from the biggest group to half the total of passengers
 
         while (noSeats <= (passengerTotal / 2)) {
-            for (int j = 0; j < groups.length; j++) {
-                if (groups[j] <= noSeats) {
-                    temp += groups[j];
+            for (int j = 0; j < numbers.length; j++) {
+                if (numbers[j] <= noSeats) {
+                    temp += numbers[j];
                 }
                 if (temp == noSeats) {
                     temp = 0;
                 }
             }
             if (temp == 0) {
-                responseArrList.add(noSeats); // Adding noSeats to responseArrList
+                response += String.valueOf(noSeats) + ','; // Adding noSeats to responseArrList
             } else {
                 temp = 0; // Restarting the temp count
             }
@@ -44,12 +43,7 @@ public class BusSize {
         }
 
         // Adding the last digit since it will always be total of passengers
-        responseArrList.add(passengerTotal);
-
-        // Changing the ArrayList into and Integer[]
-        Integer[] response = new Integer[responseArrList.size()];
-        response = responseArrList.toArray(response);
-        System.out.println(Arrays.toString(response));
+        response += String.valueOf(passengerTotal);
         return response;
     }
 }
